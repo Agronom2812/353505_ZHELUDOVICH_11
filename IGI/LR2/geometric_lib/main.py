9aff5758a71ee691d3a00215661f0b80707235a7
Jurasik <iopawelyd@gmail.com>
2025-03-24 23:40:06 +0300

    Added dotenv, dockerfile

--- IGI/LR2/geometric_lib/main.py
+++ IGI/LR2/geometric_lib/main.py
@@ -3,19 +3,10 @@ from dotenv import load_dotenv
 import square
 import circle
 
-env_path = $HOME/353505_ZHELUDOVICH_11/IGI/LR2/
+load_dotenv()
 
-load_dotenv(env_path)
-
-try:
-    r = int(os.getenv('CIRCLE_RADIUS'))
-    a = int(os.getenv('SQUARE_SIDE'))
-    
-    if r <= 0 or a <= 0:
-        raise ValueError("Radius and side length must be positive numbers")
-
-r = int(input("Circle radius(r): "))
-a = int(input("Square side length(a): "))
+r = int(os.getenv('CIRCLE_RADIUS'))
+a = int(os.getenv('SQUARE_SIDE'))
 
 circle_area = circle.area(r)
 circle_perimeter = circle.perimeter(r)
@@ -26,7 +17,3 @@ print(f"square area: {square_area}")
 print(f"square perimeter: {square_perimeter}")
 print(f"circle area: {circle_area}")
 print(f"circle perimeter: {circle_perimeter}")
-
-except (TypeError, ValueError) as e:
-    print(f"Error: {str(e)}")
-    exit(1)
